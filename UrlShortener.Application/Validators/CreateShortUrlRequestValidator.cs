f469704a4a2a4c58bacb8799618329977ed57457
Le Long <le.long.leader@gmail.com>
2025-11-13 10:25:42 +0700

    Feat: Custom Alias must be at least 8 characters and contain amd

--- UrlShortener.Application/Validators/CreateShortUrlRequestValidator.cs
+++ UrlShortener.Application/Validators/CreateShortUrlRequestValidator.cs
@@ -18,12 +18,14 @@ namespace UrlShortener.Application.Validators
                 .Must(BeAValidUrl).WithMessage("A valid URL is required.");
 
             RuleFor(x => x.CustomAlias)
-                .MinimumLength(5).When(x => !string.IsNullOrEmpty(x.CustomAlias))
-                    .WithMessage("Custom alias must be at least 5 characters.")
+                .MinimumLength(8).When(x => !string.IsNullOrEmpty(x.CustomAlias))
+                    .WithMessage("Custom alias must be at least 8 characters.")
                 .MaximumLength(30).When(x => !string.IsNullOrEmpty(x.CustomAlias))
                     .WithMessage("Custom alias must be 30 characters or less.")
                 .Matches("^[a-zA-Z0-9_-]*$").When(x => !string.IsNullOrEmpty(x.CustomAlias))
                     .WithMessage("Alias can only contain letters, numbers, dashes, and underscores.")
+                .Must(x => x.Contains("amd")). When(x => !string.isNullorEmpty(x.CustomAlias))
+                     .WithMessage("Custom alias must contain the substring 'amd'.")
                 .MustAsync(BeUniqueAlias).When(x => !string.IsNullOrEmpty(x.CustomAlias))
                     .WithMessage("This custom alias is already taken. Please choose another.");
         }
